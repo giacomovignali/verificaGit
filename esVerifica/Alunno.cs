0715a41d9214b322bd8f9f3d825ff3dfd00bac1d
giacomovignali <giacomo.vignali.stud@ispscalcomandini.it>
2022-03-18 08:24:54 +0100

    aggiunta proprieta e costruttore

--- esVerifica/Alunno.cs
+++ esVerifica/Alunno.cs
@@ -13,7 +13,38 @@ namespace esVerifica
         private int _eta;
         private string _citta;
 
-        
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public string Cognome
+        {
+            get { return _cognome; }
+            set { _cognome = value; }
+        }
+
+        public int Eta
+        {
+            get { return _eta; }
+            set { _eta = value; }
+        }
+
+        public string Citta
+        {
+            get { return _citta; }
+            set { _citta = value; }
+        }
+
+        public Alunno(string nome, string cognome, int eta, string citta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+            Citta = citta;
+        }
+
 
 
     }
