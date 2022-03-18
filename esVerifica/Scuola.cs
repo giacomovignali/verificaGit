bb26ecfa1043118a1f92f98acb323025107b9f5f
giacomovignali <giacomo.vignali.stud@ispscalcomandini.it>
2022-03-18 08:35:18 +0100

    aggiunti attributi a Scuola

---
+++ esVerifica/Scuola.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esVerifica
+{
+    class Scuola
+    {
+        private string _nome;
+        private string _indirizzo;
+        private int orarioApertura, orarioChiusura;
+
+        
+
+
+    }
+}
