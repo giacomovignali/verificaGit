a5800f8587d0cc8c80cfbb86a7458e9e5b287fa6
giacomovignali <giacomo.vignali.stud@ispscalcomandini.it>
2022-03-18 08:23:33 +0100

    aggiunta attributi Alunno

---
+++ esVerifica/Alunno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esVerifica
+{
+    class Alunno
+    {
+        private string _nome;
+        private string _cognome;
+        private int _eta;
+        private string _citta;
+
+        
+
+
+    }
+}
